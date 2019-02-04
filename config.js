441137162c2eaf96263b7ae7f93b3d4a9c310ca0
Ajith Mohan <Ajith Mohan>
2019-02-04 12:52:48 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '122119055063435',
+        clientSecret: '9832b728e290e2f67efc52ff34e12886',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
